a48a8c3cd94bc6de17663d126418bbc44f4c25e9
Thiago Mafort <thmafort@gmail.com>
2023-04-25 15:35:35 -0300

    classe que calcula preco de venda adicionada

--- Perfumaria/src/Perfumes/TestaFragrancias.java
+++ Perfumaria/src/Perfumes/TestaFragrancias.java
@@ -1,7 +1,25 @@
 package Perfumes;
 
+import java.util.Scanner;
+
 public class TestaFragrancias {
 	public static void main(String args[]) {
 		FragranciaUm f1 = new FragranciaUm();
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Formulário para preencher os detalhes da fragrancia 1\n");
+		System.out.println("Digite o nome do perfume: ");
+		f1.setNome(scanner.nextLine());
+		System.out.println("Digite a a quantidade de ml: ");
+		f1.setCapacidade(Integer.parseInt(scanner.nextLine()));
+		System.out.println("Digite o preço de custo: ");
+		f1.setPrecoDeCustoPerfume(Double.parseDouble(scanner.nextLine()));
+		System.out.println("Digite o igrediente exlusivo dessa fragrancia doce: ");
+		f1.setIngredienteDoce(scanner.nextLine());
+		System.out.println("Digite o preço de custo de materia prima da embalagem : ");
+		f1.setPrecoMateriaPrimaPorMl(Double.parseDouble(scanner.nextLine()));
+		
+		System.out.println("O preco de venda desse perfume é R$: "+f1.precoDeVenda(f1.getPrecoDeCustoPerfume()));
+		scanner.close();
 	}
 }
